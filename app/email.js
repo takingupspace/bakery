ae6e55163b8e6a5693edaeacde2c0c5ff365c9f2
takingupspace <belllabenthusiast@gmail.com>
2021-12-15 13:20:19 -0600

    fixed-email

--- app/email.js
+++ app/email.js
@@ -1,25 +1,27 @@
-require('dotenv').config()
+const { json } = require('body-parser');
 const nodemailer = require('nodemailer');
+const { getMaxListeners } = require('process');
+require('dotenv').config();
 let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port : 465,
     secure : true,
     auth : {
-        type: 'OAuth2',
-        user: process.env.EMAIL_TO,
-        pass : process.env.PASS
+        user: 't.r.s.pride@gmail.com',
+        pass : 'letskEep29S4feOK?'
     }
 })
 
 message = {
-    from: process.env.EMAIL_FROM,
-    to: process.env.EMAIL_TO,
+    from: 't.r.s.pride@gmail.com',
+    to: 't.r.s.pride@gmail.com',
     subject : 'Test Email',
     text: 'Hopefully this works so I can move on'
 }
 
 transporter.sendMail(message, function(err, info){
     if (err){
+        console.log('message.from is ' + JSON.stringify(message))
         console.log(err);
     }else{
         console.log(info);
